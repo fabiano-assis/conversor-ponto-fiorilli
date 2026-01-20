444e4bfd30acc063a16df6dfd32b37d4a24e5620
Fabiano Assis <fabianoa602@gmail.com>
2026-01-20 16:48:24 -0300

    Update conversor_ponto.py
    Refactor file conversion to group records by date and create separate output files for each date.

--- conversor_ponto.py
+++ conversor_ponto.py
@@ -2,44 +2,44 @@ import os
 import tkinter as tk
 from tkinter import filedialog, messagebox
 
-# --- CONFIGURAÇÃO ---
+
 CODIGO_EMPRESA = "001"
 
-def gerar_nome_saida(caminho_arquivo):
-    nome_original = os.path.basename(caminho_arquivo)
-    nome_sem_extensao = os.path.splitext(nome_original)[0]
-
-    nome_saida = f"{nome_sem_extensao}_convertido.txt"
-    return nome_saida
-
 def converter_arquivo_matricula(caminho_arquivo):
-    registros_convertidos = []
+    # Criação de dicionário
+    registros_por_data = {}
 
     try:
         with open(caminho_arquivo, 'r', encoding='latin-1') as f_entrada:
+            # Dividindo a linha em partes (o arquivo tem espaços em branco entre matricula, data e etc)
             for linha in f_entrada:
                 partes = linha.strip().split()
 
                 if len(partes) >= 4:
-                    data = partes[0]
-                    hora = partes[1]
+                    data = partes[0]       # 11/12/2025
+                    hora = partes[1]       # 00:00
                     matricula = partes[3]
 
                     linha_formatada = f"{CODIGO_EMPRESA} {matricula} {data} {hora}"
-                    registros_convertidos.append(linha_formatada)
+                    # Criando nova chave no dicionário para cada data diferente
+                    if data not in registros_por_data:
+                        registros_por_data[data] = []
+
+                    registros_por_data[data].append(linha_formatada)
 
         pasta_saida = os.path.dirname(caminho_arquivo)
-        nome_saida = gerar_nome_saida(caminho_arquivo)
-        caminho_saida = os.path.join(pasta_saida, nome_saida)
 
+        for data, registros in registros_por_data.items():
+            nome_saida = gerar_nome_saida_por_data(data)
+            caminho_saida = os.path.join(pasta_saida, nome_saida)
 
-        with open(caminho_saida, 'w', encoding='utf-8') as f_saida:
-            for registro in registros_convertidos:
-                f_saida.write(registro + '\n')
+            with open(caminho_saida, 'w', encoding='utf-8') as f_saida:
+                for registro in registros:
+                    f_saida.write(registro + '\n')
 
         messagebox.showinfo(
             "Sucesso",
-            f"Conversão concluída!\nArquivo gerado:\n{caminho_saida}"
+            "Conversão concluída!\nArquivos gerados por data."
         )
 
     except Exception as e:
@@ -54,7 +54,12 @@ def selecionar_arquivo():
     if arquivo:
         converter_arquivo_matricula(arquivo)
 
-# --- INTERFACE ---
+# O arquivo origem tem datas em ordem cronológica e para a importação eu preciso do agrupamento de datas por arquivo
+def gerar_nome_saida_por_data(data):
+    data_formatada = data.replace("/", "-")
+    return f"{data_formatada}.txt"
+
+# INTERFACE
 janela = tk.Tk()
 janela.title("Conversor de Ponto - Fiorilli")
 janela.geometry("400x200")
